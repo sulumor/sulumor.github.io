afabd887675ca5cd4a82f5627a80bbab14be9347
sulumor <romuald.patfoort@gmail.com>
2023-09-25 17:19:48 +0200

    changement twitter + croix du menu

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const btnSmallMenu = document.querySelector('.header__btn-container')
+const btnSmallMenu = document.querySelector('.button__hamburger')
 const smallMenu = document.querySelector('.header__content--sm-menu')
+const linksSmallMenu = document.querySelectorAll('.header__list-link--sm-menu')
 
 btnSmallMenu.addEventListener('click', () => {
   smallMenu.classList.toggle('active')
+  btnSmallMenu.classList.toggle('cross')
+})
+
+linksSmallMenu.forEach((link) => {
+  link.addEventListener('click', () => {
+    smallMenu.classList.toggle('active')
+    btnSmallMenu.classList.toggle('cross')
+  })
 })
