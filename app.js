1f756636e5b35fdaaebdd1294b4747a83fc50685
sulumor <romuald.patfoort@gmail.com>
2023-09-26 16:15:58 +0200

    fix button menu

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const smallMenu = document.querySelector('.header__content--sm-menu')
 const linksSmallMenu = document.querySelectorAll('.header__list-link--sm-menu')
 
 btnSmallMenu.addEventListener('click', () => {
-  smallMenu.style.display = 'inline-block'
+  smallMenu.classList.toggle('active')
   btnSmallMenu.classList.toggle('cross')
 })
 
