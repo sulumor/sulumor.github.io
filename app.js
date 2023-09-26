1907af1c480539da05585c017cdef0eba4bb2bbe
sulumor <romuald.patfoort@gmail.com>
2023-09-26 08:37:42 +0200

    fix button menu

--- app.js
+++ app.js
@@ -3,29 +3,13 @@ const smallMenu = document.querySelector('.header__content--sm-menu')
 const linksSmallMenu = document.querySelectorAll('.header__list-link--sm-menu')
 
 btnSmallMenu.addEventListener('click', () => {
-  if (
-    smallMenu.classList.contains('active') &&
-    btnSmallMenu.classList.contains('cross')
-  ) {
-    smallMenu.classList.remove('active')
-    btnSmallMenu.classList.remove('cross')
-  } else {
-    smallMenu.classList.add('active')
-    btnSmallMenu.classList.add('cross')
-  }
+  smallMenu.classList.toggle('active')
+  btnSmallMenu.classList.toggle('cross')
 })
 
 linksSmallMenu.forEach((link) => {
   link.addEventListener('click', () => {
-    if (
-      smallMenu.classList.contains('active') &&
-      btnSmallMenu.classList.contains('cross')
-    ) {
-      smallMenu.classList.remove('active')
-      btnSmallMenu.classList.remove('cross')
-    } else {
-      smallMenu.classList.add('active')
-      btnSmallMenu.classList.add('cross')
-    }
+    smallMenu.classList.toggle('active')
+    btnSmallMenu.classList.toggle('cross')
   })
 })
