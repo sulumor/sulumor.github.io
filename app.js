51e21b20e823c00f52c1acaad518aa4d9a00b2ca
sulumor <romuald.patfoort@gmail.com>
2023-09-27 22:44:00 +0200

    fix animation accueil + MEP page projet

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const btnSmallMenu = document.querySelector('.button__hamburger')
-console.log('test1')
 const smallMenu = document.querySelector('.header__content--sm-menu')
-console.log('test2')
 const linksSmallMenu = document.querySelectorAll('.header__list-link--sm-menu')
-console.log('test3')
+const home__cards = document.querySelectorAll('.home__card')
 
 btnSmallMenu.addEventListener('click', () => {
-  console.log('test3')
   smallMenu.classList.toggle('active')
   btnSmallMenu.classList.toggle('cross')
 })
 
 linksSmallMenu.forEach((link) => {
-  console.log('test4')
   link.addEventListener('click', () => {
     smallMenu.classList.toggle('active')
     btnSmallMenu.classList.toggle('cross')
