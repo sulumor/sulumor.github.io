531b89ca4681455860f42a7420b68fbb95494245
sulumor <romuald.patfoort@gmail.com>
2023-11-08 17:03:56 +0100

    feat: theme switch

--- app.js
+++ app.js
@@ -2,15 +2,27 @@ const btnSmallMenu = document.querySelector('.navbar-toggler')
 const smallMenu = document.querySelector('.header__content--sm-menu')
 const linksSmallMenu = document.querySelectorAll('.header__list-link--sm-menu')
 const home__cards = document.querySelectorAll('.home__card')
+const themeChanger = document.querySelector('.header__theme-switch')
 
-btnSmallMenu.addEventListener('click', () => {
-  smallMenu.classList.toggle('active')
-  btnSmallMenu.classList.toggle('cross')
+themeChanger.addEventListener('click', () => {
+  const body = document.querySelector('body')
+  const image = document.querySelector('#theme-switch-image')
+  if (!body.hasAttribute('data-theme')) {
+    body.setAttribute('data-theme', 'dark')
+    image.setAttribute('src', './pictures/icon-light.png')
+  } else {
+    body.removeAttribute('data-theme')
+    image.setAttribute('src', './pictures/icon-dark.png')
+  }
 })
+
+btnSmallMenu.addEventListener('click', actionPressSmallMenu)
 
 linksSmallMenu.forEach((link) => {
-  link.addEventListener('click', () => {
-    smallMenu.classList.toggle('active')
-    btnSmallMenu.classList.toggle('cross')
-  })
+  link.addEventListener('click', actionPressSmallMenu)
 })
+
+function actionPressSmallMenu() {
+  smallMenu.classList.toggle('active')
+  btnSmallMenu.classList.toggle('cross')
+}
