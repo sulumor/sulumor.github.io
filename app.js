02c9e3c9d3edafe59f7ce4a692bea2460e47e624
sulumor <romuald.patfoort@gmail.com>
2023-11-16 18:20:55 +0100

    feat:check mode theme user

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 const btnSmallMenu = document.querySelector('.navbar-toggler')
 const smallMenu = document.querySelector('.header__content--sm-menu')
 const linksSmallMenu = document.querySelectorAll('.header__list-link--sm-menu')
-const home__cards = document.querySelectorAll('.home__card')
-const themeChanger = document.querySelector('.header__theme-switch')
+const body = document.querySelector('body')
 
-themeChanger.addEventListener('click', () => {
-  const body = document.querySelector('body')
-  const image = document.querySelector('#theme-switch-image')
-  if (!body.hasAttribute('data-theme')) {
-    body.setAttribute('data-theme', 'dark')
-    image.setAttribute('src', './pictures/icon-light.png')
-  } else {
-    body.removeAttribute('data-theme')
-    image.setAttribute('src', './pictures/icon-dark.png')
-  }
-})
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  body.setAttribute('data-theme', 'dark')
+} else {
+  body.removeAttribute('data-theme')
+}
 
 btnSmallMenu.addEventListener('click', actionPressSmallMenu)
 
-linksSmallMenu.forEach((link) => {
-  link.addEventListener('click', actionPressSmallMenu)
-})
+linksSmallMenu.forEach(link => link.addEventListener('click', actionPressSmallMenu))
 
 function actionPressSmallMenu() {
   smallMenu.classList.toggle('active')
