2ba38a2e5fa2b797b65d169973a36124ed9c23c1
sulumor <romuald.patfoort@gmail.com>
2023-11-21 13:38:41 +0100

    add comments in JS

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const btnSmallMenu = document.querySelector('.navbar-toggler')
-const smallMenu = document.querySelector('.header__content--sm-menu')
-const linksSmallMenu = document.querySelectorAll('.header__list-link--sm-menu')
-const body = document.querySelector('body')
+const btnSmallMenu = document.querySelector('.navbar-toggler');
+const smallMenu = document.querySelector('.header__content--sm-menu');
+const linksSmallMenu = document.querySelectorAll('.header__list-link--sm-menu');
+const body = document.querySelector('body');
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  body.setAttribute('data-theme', 'dark')
-} else {
-  body.removeAttribute('data-theme')
-}
+//Voir quel est le thème de la machine
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) 
+  body.setAttribute('data-theme', 'dark');
+else body.removeAttribute('data-theme');
 
-btnSmallMenu.addEventListener('click', actionPressSmallMenu)
 
-linksSmallMenu.forEach(link => link.addEventListener('click', actionPressSmallMenu))
-
+//toggle du button menu Mobile
 function actionPressSmallMenu() {
   smallMenu.classList.toggle('active')
   btnSmallMenu.classList.toggle('cross')
 }
+
+btnSmallMenu.addEventListener('click', actionPressSmallMenu);
+linksSmallMenu.forEach(link => link.addEventListener('click', actionPressSmallMenu));
