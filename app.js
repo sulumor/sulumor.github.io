57db095522010d9c72f2c41fc7b7c17de5ea31bc
sulumor <romuald.patfoort@gmail.com>
2024-02-01 22:11:35 +0100

    feat: add sending contact form

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const btnSmallMenu = document.querySelector('.navbar-toggler');
 const smallMenu = document.querySelector('.header__content--sm-menu');
 const linksSmallMenu = document.querySelectorAll('.header__list-link--sm-menu');
 const body = document.querySelector('body');
+const form = document.querySelector('#my-form');
 
 //Voir quel est le thème de la machine
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) 
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
   body.setAttribute('data-theme', 'dark');
 else body.removeAttribute('data-theme');
 
@@ -17,3 +18,26 @@ function actionPressSmallMenu() {
 
 btnSmallMenu.addEventListener('click', actionPressSmallMenu);
 linksSmallMenu.forEach(link => link.addEventListener('click', actionPressSmallMenu));
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const status = document.querySelector("#my-form-status");
+  const data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Merci pour votre message! Je vais vous répondre très prochainement.";
+      form.reset()
+    } else {
+      status.innerHTML = "Oops! Il s'est passé un problème dans l'envoie de votre message"
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! Il s'est passé un problème dans l'envoie de votre message"
+  });
+}
+form.addEventListener("submit", handleSubmit)
